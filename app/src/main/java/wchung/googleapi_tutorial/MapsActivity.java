23580fe3171b05ec7f2fad4fda3862e7017c166e
t00037035 <wchung@gmail.com>
2017-02-15 10:19:09 -0800

    added location marker

--- app/src/main/java/wchung/googleapi_tutorial/MapsActivity.java
+++ app/src/main/java/wchung/googleapi_tutorial/MapsActivity.java
@@ -1,19 +1,52 @@
 package wchung.googleapi_tutorial;
 
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
-
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1; //request location
+    private Polygon polygon;
+    private PolygonOptions rectOptions;
+    Boolean crossed = false; //when user crosses location fence change colour
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private Marker mCurrLocationMarker;
+    double latitude;
+    double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +57,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //stop location updates when Activity is no longer active
+        if (mGoogleApiClient != null) {
+            //move app to the background to not waste battery
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,10 +80,191 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        // Add a marker in TRU and move the camera
+        LatLng kamloops = new LatLng(50.67046254, -120.3623406);
 
-        // Add a marker in Sydney and move the camera
-        LatLng OM = new LatLng(50.671645,-120.362740);
-        mMap.addMarker(new MarkerOptions().position(OM).title("TRU"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(OM));
+        //create a marker on the map
+        mMap.addMarker(new MarkerOptions().position(kamloops).title("Marker in Kamloops 1")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ghost)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kamloops, 17)); //17 is the zoom amount
+
+        // Add a polygon
+        rectOptions = new PolygonOptions()
+                .add(   new LatLng(50.67065191, -120.3619401),
+                        new LatLng(50.67065191, -120.3627411),
+                        new LatLng(50.67027317, -120.3627411),
+                        new LatLng(50.67027317, -120.3619401));
+
+        //when we cross the fence it will change the colour of the rectangle
+        if (crossed == true) {
+            rectOptions.strokeColor(-65536);
+            rectOptions.fillColor(Color.argb(20, 255, 80, 255));
+        }
+        // Get back the mutable Polygon
+        polygon = mMap.addPolygon(rectOptions);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) && ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                Log.d("Log", "#1 INFO: shouldShowRequestPermissionRationale");
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                showMessageOKCancel("You need to allow access to your location. Otherwise, you can't use the app!");
+
+            } else {
+
+                Log.d("Log", "#2 INFO: request the permission - No explanation needed");
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+
+            //Permission granted
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    private void showMessageOKCancel(String message) {
+        Log.d("Log", "#5 INFO: call AlertDialog");
+
+        new AlertDialog.Builder(MapsActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("Cancel", listener)
+                .create()
+                .show();
+    }
+
+
+    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+
+        final int BUTTON_NEGATIVE = -2;
+        final int BUTTON_POSITIVE = -1;
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case BUTTON_NEGATIVE:
+                    // int which = -2
+                    Log.d("Log", "#7 INFO: permission is denied for the second time!");
+
+                    dialog.dismiss();
+                    break;
+
+                case BUTTON_POSITIVE:
+                    // int which = -1
+                    Log.d("Log", "#6 INFO: send a second request");
+
+                    ActivityCompat.requestPermissions(
+                            MapsActivity.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_LOCATION);
+                    dialog.dismiss();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.d("Log", "#3 INFO: received a response permission was granted");
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    } else {
+                        buildGoogleApiClient();
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    Log.d("Log", "#4 INFO: received a response permission - denied");
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if (location != null) {
+            //mLastLocation = location;
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            if (latitude <= 50.67065191 && latitude >= 50.67027317 && longitude <= -120.3619401 && longitude >= -120.3627411){
+                Toast.makeText(getBaseContext(),"Current Location: Lat = " + latitude + ", and longitude = " + longitude, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
